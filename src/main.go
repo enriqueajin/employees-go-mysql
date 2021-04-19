3b4755eec6aa0f8d4642745f2367680b0ac09b55
Enrique Ajin <enriquesaon@gmail.com>
2021-04-19 13:43:31 -0600

    setting up the MySQL server using Docker to expose the port 3306 to the localhost. employees database was created in the MySQL container. Gorm, go-sql-driver and dialects libraries were installed. Code added to main.go to open and close the database connection.

--- src/main.go
+++ src/main.go
@@ -5,21 +5,32 @@ import (
 	"net/http"
 	"github.com/gorilla/mux"
 	log "github.com/sirupsen/logrus"
+	_ "github.com/go-sql-driver/mysql"
+       "github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
+// Responses to client API is Ok
 func Healthz(w http.ResponseWriter, r *http.Request) {
 	log.Info("API Health is OK")
 	w.Header().Set("Content-Type", "application/json")
 	io.WriteString(w, `{"alive": true}`)
 }
 
+// Set up the logger settings
 func init() {
 	log.SetFormatter(&log.TextFormatter{})
 	log.SetReportCaller(true)
 }
 
+var db, _ = gorm.Open("mysql", "root:root@/employees?charset=utf8&parseTime=True&loc=Local")
+
+// Main function
 func main() {
-	log.Info("Starting Todolist API server")
+	// Close database connection
+	defer db.Close()
+
+	log.Info("Starting employees API server")
 	router := mux.NewRouter()
 	router.HandleFunc("/healthz", Healthz).Methods("GET")
 	http.ListenAndServe(":8000", router)
