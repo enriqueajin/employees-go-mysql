4a0c314932c7f1e9d21faea8b48fb515857bc5ec
Enrique Ajin <enriquesaon@gmail.com>
2021-04-19 15:55:10 -0600

    CreateEmployee function created to insert data into the database. 'create' router added with the method POST.

--- src/main.go
+++ src/main.go
@@ -8,6 +8,8 @@ import (
 	_ "github.com/go-sql-driver/mysql"
        "github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"strconv"
+	"encoding/json"
 )
 
 // Responses to client API is Ok
@@ -31,6 +33,21 @@ type EmployeesModel struct {
 	Salary float64
 }
 
+func CreateEmployee(w http.ResponseWriter, r *http.Request) {
+	firstName := r.FormValue("firstname")
+	log.WithFields(log.Fields{"firstname": firstName}).Info("First name added")
+	lastName := r.FormValue("lastname")
+	log.WithFields(log.Fields{"lastname": lastName}).Info("Last name added")
+	var salary string = r.FormValue("salary")
+	log.WithFields(log.Fields{"salary": salary}).Info("Salary added")
+	sal, _ := strconv.ParseFloat(salary, 64)
+	employee := &EmployeesModel{FirstName: firstName, LastName: lastName, Salary: sal }
+	db.Create(&employee)
+	result := db.Last(&employee)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(result.Value)
+}
+
 // Open database connection
 var db, _ = gorm.Open("mysql", "root:root@/employees?charset=utf8&parseTime=True&loc=Local")
 
@@ -46,5 +63,6 @@ func main() {
 	log.Info("Starting employees API server")
 	router := mux.NewRouter()
 	router.HandleFunc("/healthz", Healthz).Methods("GET")
+	router.HandleFunc("/create", CreateEmployee).Methods("POST")
 	http.ListenAndServe(":8000", router)
 }
